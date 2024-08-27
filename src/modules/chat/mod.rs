cd1c7e626994059b8d5c62ece7a2620dd88280cd
kvltyapka <kvltyapka@gmail.com>
2024-08-27 16:57:24 +0300

    Add text extraction from documents for bot

--- src/modules/chat/mod.rs
+++ src/modules/chat/mod.rs
@@ -42,6 +42,13 @@ use async_std::io;
 use async_std::prelude::*;
 use std::time::Instant;
 
+use pdf_extract::extract_text_from_mem;
+use quick_xml::events::Event;
+use quick_xml::Reader;
+use std::io::Cursor;
+use std::io::Read;
+use zip::ZipArchive;
+
 pub(crate) use session::Session;
 pub(crate) use session_mgr::SessionManager;
 
@@ -49,6 +56,68 @@ use super::openai::get_msg_content;
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 struct MessageText(String);
+
+// TODO: Fix the issue where the bot only sees text from a file/image but doesn't see text from the message
+fn extract_text_from_document(buffer: &[u8], file_name: &str) -> Option<String> {
+    match file_name {
+        name if name.ends_with(".pdf") => extract_text_from_pdf(buffer),
+        name if name.ends_with(".docx") => extract_text_from_docx(buffer),
+        name if name.ends_with(".txt") => Some(String::from_utf8_lossy(buffer).to_string()),
+        _ => None,
+    }
+}
+
+fn extract_text_from_pdf(buffer: &[u8]) -> Option<String> {
+    let result = extract_text_from_mem(buffer);
+    if result.is_ok() {
+        return Some(result.unwrap());
+    } else {
+        info!("{}", result.unwrap_err());
+        return None;
+    }
+}
+
+fn extract_text_from_docx(buffer: &[u8]) -> Option<String> {
+    let cursor = Cursor::new(buffer);
+    let mut zip = ZipArchive::new(cursor).ok()?;
+    let mut document_xml = String::new();
+
+    for i in 0..zip.len() {
+        let mut file = zip.by_index(i).ok()?;
+        if file.name().ends_with("word/document.xml") {
+            let mut buffer = Vec::new();
+            file.read_to_end(&mut buffer).ok()?;
+            document_xml = String::from_utf8_lossy(&buffer).to_string();
+            break;
+        }
+    }
+
+    let mut text = String::new();
+    let mut reader = Reader::from_str(&document_xml);
+    let mut buf = Vec::new();
+
+    loop {
+        let event = reader.read_event_into(&mut buf);
+        if event.is_ok() {
+            let e = event.unwrap();
+            if let Event::Text(e) = e {
+                text.push_str(&e.unescape().unwrap_or_default().trim());
+            } else if let Event::Eof = e {
+                break;
+            }
+        } else {
+            info!("{}", event.unwrap_err());
+            break;
+        }
+        buf.clear();
+    }
+
+    if text.is_empty() {
+        return None;
+    } else {
+        return Some(text);
+    }
+}
 
 async fn handle_chat_message(
     bot: Bot,
@@ -67,6 +136,26 @@ async fn handle_chat_message(
     if text.starts_with('/') {
         // Let other modules to process the command.
         return false;
+    }
+
+    // Documents handling
+    if let Some(document) = msg.document() {
+        match bot.get_file(document.file.id.clone()).send().await {
+            Ok(file) => {
+                let mut buffer: Vec<u8> = Vec::new();
+                match bot.download_file(&file.path, &mut buffer).await {
+                    Ok(_) => {
+                        text = extract_text_from_document(
+                            &buffer,
+                            document.file_name.as_deref().unwrap_or_default(),
+                        )
+                        .unwrap_or_default();
+                    }
+                    Err(err) => log::warn!("{}", err),
+                }
+            }
+            Err(err) => log::warn!("{}", err),
+        }
     }
 
     // Image handling
